3dbe819fa20d4f31dc2ed4ba55d3716f709c9b9f
JulianoKosloski <juliano.koslos@gmail.com>
2024-09-16 21:41:22 -0300

    base do app

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      titulo: "Login",
+      textoLabel: "",
+      username: "",
+      password: ""
+    }
+
+    this.validar = this.validar.bind(this);
+    this.alterarSenha = this.alterarSenha.bind(this);
+    this.alterarUsername = this.alterarUsername.bind(this);
+  }
+
+  validar(){
+    let valido = false;
+    let state = this.state;
+
+    if (state.username === "juliano.koslos@gmail.com" && state.password === "123456") {
+      valido = true;
+    }
+
+    if (valido) {
+      state.textoLabel = "Acessado com sucesso!"
+    } else {
+      state.textoLabel = "Usuário ou senha incorretos!"
+    } 
+
+    this.setState(state);
+  }
+
+  alterarUsername(event) {
+    let state = this.state;
+    state.username = event.target.value;
+    this.setState(state);
+  }
+
+  alterarSenha(event) {
+    let state = this.state;
+    state.password = event.target.value;
+    this.setState(state);
+  }
+
+  render() {
+    return(
+      <div>
+        <Titulo texto={this.state.titulo}></Titulo>
+        <input type="text" size="20" name="username" onChange={(e) => this.alterarUsername(e)}></input>
+        <br></br>
+        <input type="password" size="20" name="password" onChange={(e) => this.alterarSenha(e)}></input>
+        <br></br>
+        <button onClick={this.validar}>Acessar</button>
+        <br></br>
+        <LabelLogin texto={this.state.textoLabel}></LabelLogin>
+      </div>
+    );
+  } 
+
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function Titulo(props) {
+  return (
+    <h1>{props.texto}</h1>
+  );
+}
+
+function LabelLogin(props) {
+  return (
+    <label>{props.texto}</label>
+  );
+}
+
+
+export default App;
